50ab93b6ab5dd2ccabea186b84dbc3bc9b9eee42
Scott Blamey <scott151197@msn.com>
2023-09-17 11:30:54 +0200

    parser now working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
 import * as cheerio from "cheerio";
 import chalk from "chalk";
-const highVis = chalk.red.bold.bgWhite;
+const highVis = chalk.bgYellow;
 const errVis = chalk.white.bold.bgRed;
 
 async function read(path) {
@@ -13,32 +13,6 @@ async function read(path) {
     }
 }
 
-/*
-<div id="0268C394-D6DC-5AAA-843C-E7199A3A9550" class="bookmark bm-color-none">
-      <p class="bm-page">22</p>
-      <div class="bm-text">
-            <p>To understand the meaning of this afterlife, you must remember that everyone is multifaceted. And since you always lived inside your own head, you were much better at seeing the truth about others than you ever were at seeing yourself. So you navigated your life with the help of others who held up mirrors for you. People praised your good qualities and criticized your bad habits, and these perspectives—often surprising to you—helped you to guide your life. So poorly did you know yourself that you were always surprised at how you looked in photographs or how you sounded on voice mail</p>
-      </div>
-      <div class="bm-note">
-            <p>tour perception of yourself is entirely a reflection of others perceptions of you. if this is true then what are you other than the perceptions others have of you and if others hold no perceptions of you then what are you? do you exist?</p>
-      </div>
-    </div>
-<div>
-
-<div>
-    <p>page Number<p>
-    <div>
-        <p>qutoe<p>
-    <div>
-    <div>
-        <p>comment<p>
-    <div>
-<div>
-            $("div:has(p):eq(19) > p:first").text(),
-            $("div:has(p):eq(19)").children().eq(2).text(),
-            $("div:has(p):eq(19)").children().eq(3).text()
-*/
-
 function htmlParser(data) {
     const $ = cheerio.load(data);
 
@@ -49,25 +23,33 @@ function htmlParser(data) {
         this.comment = comment;
     }
 
-    const newData = [];
+    const parsedData = [];
 
     for (let i = 0; $("div:has(p):eq(" + i + ")").length > 0; i++) {
-        let page = `"div:has(p):eq(${i}) > p:first"`;
-        let quote = `"div:has(p):eq(${i})"`;
-        let comment = `"div:has(p):eq(${i})"`;
+        let testObj = {};
+        let page = "div:has(p):eq(" + i + ") > p:first";
+        let quote = "div:has(p):eq(" + i + ")";
+        let comment = "div:has(p):eq(" + i + ")";
 
-        testObj = new objConstructor( //calling constructor function
+        testObj = new objConstructor(
             $(page).text(),
             $(quote).children().eq(2).text(),
             $(comment).children().eq(3).text()
         );
-        newData.push(testObj);
-    }
-    console.log(newData);
+        parsedData.push(testObj);
 
-    // console.log(chalk.bgGray.yellow("TESTOBJ.PAGE: ") + testObj.page);
-    // console.log(
-    //     chalk.bgGray.yellow("EXISTS?: ") + $("div:has(p):eq(40)").length
+        // console.log(
+        //     highVis.bold(`parsedData.page >     ${i}:`) +
+        //         `${parsedData[i].page}`
+        // );
+        // console.log(
+        //     highVis(`parsedData.quote >    ${i}:`) + `${parsedData[i].quote}`
+        // );
+        // console.log(
+        //     highVis(`parsedData.comment >  ${i}:`) + `${parsedData[i].comment}`
+        // );
+    }
+    return parsedData;
 }
 
 async function execute(newName, newPath, newData) {
@@ -78,10 +60,10 @@ async function execute(newName, newPath, newData) {
     }
 }
 
-async function main(inputPath /*outputFileName*/) {
+async function main(inputPath, outputFileName) {
     const outputData = htmlParser(await read(inputPath));
 
-    //await execute(outputFileName, outputData);
+    await execute(outputFileName, outputData);
 }
 
 main("Sum Forty Tales from the Afterlives.html", "outputData.md");
