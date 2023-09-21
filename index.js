9e4fb486e5c37bd63ae0e0e40f04aa0d23f1f6e3
Scott Blamey <scott151197@msn.com>
2023-09-21 09:34:29 +0200

    string constructor and execute now working

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+"use strict";
 import fs from "fs/promises";
 import * as cheerio from "cheerio";
 import chalk from "chalk";
 const highVis = chalk.bgYellow;
 const errVis = chalk.white.bold.bgRed;
 
-async function read(path) {
+async function read(inputPath) {
     try {
-        const data = await fs.readFile(path, { encoding: "utf8" });
+        const data = await fs.readFile(inputPath, { encoding: "utf8" });
         return data;
     } catch (error) {
         console.log(errVis("'Read' Function Error") + { error });
     }
 }
-const parsedData = [];
 
 function htmlParser(data) {
     const $ = cheerio.load(data);
+    const parsedData = [];
 
     function objConstruct(page, quote, comment) {
         this.page = page;
@@ -23,7 +24,11 @@ function htmlParser(data) {
         this.comment = comment;
     }
 
-    for (let i = 0; i < 10; i++) {
+    for (
+        let i = 0;
+        $("div.bookmark:eq(" + i + ") > .bm-page").length !== 0;
+        i++
+    ) {
         let obj = {};
         let $page = "div.bookmark:eq(" + i + ") > .bm-page";
         let $quote = "div.bookmark:eq(" + i + ") > .bm-text > p";
@@ -36,26 +41,34 @@ function htmlParser(data) {
         );
         parsedData.push(obj);
     }
+    return parsedData;
 }
 
 function strConstruct(data) {
-    let str = `###Book 1 /n Page: ${parsedData[9].page} /n Quote: ${parsedData[9].quote} /n Comment: ${parsedData[9].comment}`;
-
+    console.log(`bla bla ${data[8].page}`);
+    //return `bla bla ${data[1].quote}`;
+    //console.log(data[9].page);
+    let str = `### Book 1 \n\n Page: ${data[3].page} \n Quote: ${data[3].quote} \n Comment: ${data[3].comment}`;
     return str;
 }
 
-async function execute(newName, newPath, newData) {
+async function execute(newFile, newData, encoding) {
     try {
-        fs.writeFile(newName, newPath, newData);
+        fs.writeFile(newFile, newData, encoding);
     } catch (error) {
         console.log(errVis("'Execute' Function Error") + { error });
     }
 }
 
-async function main(inputPath, outputFileName) {
-    const outputData = htmlParser(await read(inputPath));
+async function main() {
+    const parsedData = htmlParser(
+        await read(".\\Sum Forty Tales from the Afterlives.html")
+    );
+    const outputData = strConstruct(parsedData);
 
-    await execute(outputFileName, ".\\", strConstruct(parsedData));
+    console.log(chalk.yellow(parsedData[1].quote));
+
+    await execute(".\\outputData.md", outputData, "utf-8");
 }
 
-main("Sum Forty Tales from the Afterlives.html", "outputData.md");
+main();
